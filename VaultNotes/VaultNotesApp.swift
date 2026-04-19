5b3970b72ad3356e2c34069977ba868128c13937
byt23 <berkayyasar2004@gmail.com>
2026-04-19 14:48:24 +0300

    Initial Commit

---
+++ VaultNotes/VaultNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultNotesApp.swift
+//  VaultNotes
+//
+//  Created by BERKAY TURAN on 19.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct VaultNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
